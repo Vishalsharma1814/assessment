c4eb6836e3cb423d594cd6d907a0b90cedebdef7
Vishal Sharma <VSharma@bpl.netlink.com>
2022-08-22 23:34:09 +0530

    Changes

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import BarChart from './components/BarChart';
+import ScatterPlot from './components/ScatterPlot';
+
+
+function App() {
+ return (
+  <div className='container'>
+  <BarChart/>
+  <hr />
+  <ScatterPlot/>
+  </div>
+ )
+}
+
+export default App;
